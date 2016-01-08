c198f5ebf48256781dc0c3a22f485080a6b27b87
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-01-08 15:20:40 +0700

    apply handlebars template engine

--- chap3/meadowlark.js
+++ chap3/meadowlark.js
@@ -2,31 +2,35 @@ var express = require('express'),
 		app = express()
 ;
 
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+								.create({defaultLayout: 'main'})
+;
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 9000);
 
 app.get('/', function(req, res) {
-	res.type('text/plain');
-	res.send('Meadowlark Travel');
+	res.render('home');
 });
 
 app.get('/about', function(req, res) {
-	res.type('text/plain');
-	res.send('About Meadowlark Travel');
+	res.render('about');
 });
 
 // custom 404 page
 app.use(function(req, res) {
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next) {
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Internal Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function() {
