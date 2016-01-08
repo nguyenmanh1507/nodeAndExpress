f1e5a0a655056490aed16bc1ff9558d6249f0cac
nguyen manh <nguyenmanh1507@gmail.com>
2016-01-08 21:28:47 +0700

    dynamic content in Views

--- chap3/meadowlark.js
+++ chap3/meadowlark.js
@@ -7,17 +7,28 @@ var handlebars = require('express3-handlebars')
 								.create({defaultLayout: 'main'})
 ;
 
+var fortunes = [
+	'Conquer your fear or they will conquer you',
+	'Rivers need spring',
+	'Do not fear what you don\'t know',
+	'You will have a pleasant surprise',
+	'Whenever possible, keep it simple'
+];
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 9000);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	res.render('home');
 });
 
 app.get('/about', function(req, res) {
-	res.render('about');
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', {fortune: randomFortune});
 });
 
 // custom 404 page
