302d7ec8ac0ac8fd0f46f1e58b8d313ec225ded5
manh <nguyenmanh1507@gmail.com>
2016-09-13 12:51:18 +0700

    handle form submit

--- form/server.js
+++ form/server.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
 
 app.set('view engine', 'pug')
 
-app.use(require('body-parser')())
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(require('body-parser').urlencoded({
+  extended: true
+}))
 
 app.get('/', (req, res) => {
   console.log(req.body)
   res.render('index')
 })
 
-// app.get('/pro')
+// Newsletter route
+app.get('/newsletter', (req, res) => {
+  res.render('newsletter', {csrf: 'CSRF token goes here!'})
+})
+
+// Process route
+app.get('/process', (req, res) => {
+  console.log(`Form (from querystring): ${req.query.form}`)
+  console.log(`CSRF token (from hidden form field): ${req.body._csrf}`)
+  console.log(`Name (from visible form field): ${req.body.name}`)
+  console.log(`Email (from visible form field): ${req.body.email}`)
+  res.redirect(303, '/thank-you')
+})
 
 app.listen(3000)
